e8ef4cc96068b30a5c4e4f2a88805a63e27213bc
Sion GENDERS <sion.genders@estiam.com>
2019-01-09 11:08:04 +0100

    Edit fait

--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,48 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
+// https://github.com/estiam/E4C1718ELK
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   mongoose.model('Movie').find({}, (err, items) => {
-    if(err)
+    if (err)
       return res.send(err);
 
     res.render('index', { title: 'Express', movies: items });
   });
 });
 
-router.get('/create', function(req, res, next) {
+router.get('/create', function (req, res, next) {
   res.render('create');
 });
 
-router.post('/create', function(req, res, next) {
+router.post('/create', function (req, res, next) {
   const film = req.body;
 
-  mongoose.model('Movie').create(film, function(err, item) {
-    if(!err)
+  mongoose.model('Movie').create(film, function (err, item) {
+    if (!err)
       return res.redirect('/');
     res.send(err);
   });
 });
 // router.get .post .put .delete .head etc...
 
-router.get('/edit', function(req, res, next) {
-  res.render('edit');
+router.get('/edit/:id', function (req, res, next) {
+  mongoose.model('Movie').findById(req.params.id, function (err, item) {
+    if (err)
+      return res.send(err);
+
+    res.render('edit', { film: item });
+  });
 });
 
-router.get('/edit', function(req, res, next) {
-  res.render('edit');
+router.post('/edit/:id', function (req, res, next) {
+  mongoose.model('Movie').findByIdAndUpdate(req.params.id, req.body, function (err, item) {
+    if (!err)
+      return res.redirect('/');
+
+    res.send(err);
+  });
 });
 module.exports = router;
